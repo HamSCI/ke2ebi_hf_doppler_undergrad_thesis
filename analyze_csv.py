13f0cb34460a06f5ce114e8462b0aedc43eb7eac
Rebecca <rebecca.potter2023@gmail.com>
2026-01-09 17:19:18 -0500

    convert decimal date to UTC format

--- analyze_csv.py
+++ analyze_csv.py
@@ -5,6 +5,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+import datetime
+import math
 
 station = 'W2NAF'
 
@@ -15,6 +17,7 @@ output_dir     = os.path.join(base_directory,'output')
 filename = 'ACF_FWL_data__15.0MHz_2024-05-10.csv'
 filepath = os.path.join(data_dir, filename)
 
+# Pull frequency and date from filename
 match = re.search(r'(\d+\.?\d*)MHz_(\d{4}-\d{2}-\d{2})', filename)
 if match:
     frequency = f"{match.group(1)} MHz"
@@ -28,6 +31,7 @@ if not os.path.exists(filepath):
 else: 
     print(filepath)
 
+# Set output directory to save plots in and name output .png
 plot_dir = os.path.join(output_dir,'plots',station)
 os.makedirs(plot_dir,exist_ok=True)
 outfile  = os.path.join(
@@ -35,6 +39,7 @@ outfile  = os.path.join(
     f"{station}_Doppler_vs_time_{frequency.replace(' ','')}_{date}.png"
 )
 
+# Read .csv
 data = np.genfromtxt(
     filepath,
     delimiter=',',
@@ -60,7 +65,7 @@ t_max = hour[index_max]
 print(f'Minimum doppler: {doppler_min} at {t_min}')
 print(f'Maximum doppler: {doppler_max} at {t_max}')
 
-plt.plot(hour,doppler)
+plt.scatter(hour,doppler, s=3.5,c='black')
 plt.title(f'Doppler Shift vs. Time\nFrequency: {frequency} | Date: {date} ')
 plt.xlabel('Hour (UTC)')
 plt.ylabel('Doppler Shift (Hz)')
@@ -68,5 +73,24 @@ plt.gcf().set_size_inches(8, 3, forward=True)
 plt.tight_layout()
 plt.savefig(outfile,dpi=300)
 plt.show()
+
+# Convert decimal hours to an actual UTC time
+def decimal_hours_to_utc(h):
+   hours = int(h)
+   minutes = int((h-hours)*60)
+   seconds = round((((h-hours)*60)-minutes)*60)
+   if seconds == 60:
+    seconds = 0
+    minutes += 1
+   if minutes == 60:
+      minutes = 0
+      hours += 1
+   if hours == 24:
+      hours = 0
+
+   return datetime.time(hour=hours,minute=minutes,second=seconds)
+
+for t in hour:
+    print(decimal_hours_to_utc(t))
 
 
